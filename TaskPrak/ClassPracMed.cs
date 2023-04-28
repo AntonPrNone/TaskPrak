e427be69667893986ffb7095dd57acb066d51f9c
201904 <201904@TOCHKA-003>
2023-04-28 15:44:36 +0300

    Добавьте файлы проекта.

---
+++ TaskPrak/ClassPracMed.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TaskPrak
+{
+    internal class ClassPracMed
+    {
+        public static void HandleError(Exception ex) // Метод отлова / оповещения об ошибке
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // Оповещаем пользователя об ошибке
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+
+        public static string FixRegistrText(string text) // Метод исправления текста (Регистр)
+        {
+            StringBuilder result = new StringBuilder(text.ToLower()); // Делаем все буквы маленькими
+            bool startOfSentence = true;  // Флаг, указывающий на начало предложения
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (startOfSentence && Char.IsLetter(result[i]))
+                {
+                    result[i] = Char.ToUpper(result[i]);
+                    startOfSentence = false;
+                }
+                else if (result[i] == '.' || result[i] == '!' || result[i] == '?')
+                {
+                    startOfSentence = true;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static string FixSyntaxText(string text)
+        {
+            // Разбиваем текст на предложения
+            string[] sentences = text.Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Исправляем пробелы в каждом предложении и склеиваем предложения обратно в текст
+            StringBuilder result = new StringBuilder();
+            int endIndex = 0;
+            foreach (string sentence in sentences)
+            {
+                string trimmedSentence = sentence.Trim();
+                if (trimmedSentence.Length > 0)
+                {
+                    // Заменяем последовательности пробелов на один пробел
+                    string processedSentence = Regex.Replace(trimmedSentence, @"\s+", " ");
+                    result.Append(processedSentence);
+
+                    // Добавляем знак припинания, если он был в исходном тексте
+                    endIndex = text.IndexOf(sentence, endIndex) + sentence.Length;
+                    if (endIndex < text.Length)
+                    {
+                        result.Append(text[endIndex]);
+                        result.Append(' ');
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ TaskPrak/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TaskPrak.ClassPracMed;
+
+namespace TaskPrak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   
+                //var a = new int[] { 1, 2, 3 };
+                //int b = a[4];
+                // Код, который может вызвать ошибку
+            }
+            catch (Exception ex)
+            {
+                // Отлавливаем ошибку и обрабатываем ее
+                HandleError(ex);
+            }
+
+            // -----------------------------------------------------------------------------------------------------------------------
+
+            string text = "Это Текст    с НеКоРРектным регистрОм.   Он нуждается в исправлении ! Кто-то может помочь?";
+            string fixedText = FixSyntaxText(FixRegistrText(text));
+            Console.WriteLine(fixedText); // "Это Текст С Некорректным Регистром. Он Нуждается В Исправлении! Кто-То Может Помочь?"
+
+            Console.ReadLine();
+        }
+    }
+}
